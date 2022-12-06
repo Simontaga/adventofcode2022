8ae3381ef8360e2d2d9d2c725d5ec327fd13ed5c
Simon Törnlund <simon.tornlund@ecsolutions.se>
2022-12-06 20:12:30 +0100

    Less is more

--- days_rest/src/days/day_6.rs
+++ days_rest/src/days/day_6.rs
@@ -1,25 +1,21 @@
-pub fn day6_part_1() -> i32 {
-    get_index_unique(4)
-}
+use std::collections::HashSet;
 
-pub fn day6_part_2() -> i32 {
-    get_index_unique(14)
-}
+pub fn day6_part_1() -> i16 { get_index_unique(4) }
 
-fn get_index_unique(target: usize) -> i32 {
+pub fn day6_part_2() -> i16 { get_index_unique(14) }
+
+fn get_input() -> String { std::fs::read_to_string("src/days/input/6.txt").unwrap() }
+
+
+fn get_index_unique(target: usize) -> i16 {
     let characters: Vec<char> = get_input().chars().collect();
 
     for i in 0..characters.len() {
-        let mut window: Vec<char> = characters[i..i + target].to_vec();
-        window.sort_unstable();
-        window.dedup();
+        let window:HashSet<char> = HashSet::from_iter(characters[i..i + target].to_vec());
         if window.len() != target { continue; }
-        return (i + target) as i32;
+        return (i + target) as i16
     }
 
     0
 }
 
-fn get_input() -> String {
-    std::fs::read_to_string("src/days/input/6.txt").unwrap()
-}
