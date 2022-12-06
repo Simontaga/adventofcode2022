7a034a608cd9a0e1bcef3a94d099fe9d80e16903
Simon Törnlund <simon.tornlund@ecsolutions.se>
2022-12-06 19:28:23 +0100

    Refactor

--- days_rest/src/days/day_6.rs
+++ days_rest/src/days/day_6.rs
@@ -1,35 +1,32 @@
 pub fn day6_part_1() -> i32 {
     let data = get_input();
-    return get_index_unique(data, 4);
+    get_index_unique(data, 4)
 }
 
 fn get_index_unique(string: String, target: usize) -> i32 {
-    let mut ans = 0;
     let mut unique: Vec<char> = Vec::new();
-    let lookup_copy: Vec<char> = string.chars().collect();
+    let characters: Vec<char> = string.chars().collect();
 
-    for (i, _) in string.chars().into_iter().enumerate() {
+    for i in 0..characters.len() {
         for k in 0..target {
-            unique.push(*lookup_copy.get(i + k).unwrap());
+            unique.push(*characters.get(i + k).unwrap());
         }
-        
-        unique.sort();
+
+        unique.sort_unstable();
         unique.dedup();
 
         if unique.len() == target {
-            ans = i+target;
-            break;
+            return (i + target) as i32;
         }
         unique.clear();
     }
 
-    ans as i32
+    0
 }
-
 
 pub fn day6_part_2() -> i32 {
     let data = get_input();
-    return get_index_unique(data, 14);
+    get_index_unique(data, 14)
 }
 
 fn get_input() -> String {
