05791e579510a05d52939715efab813f2b5d3809
Simon Törnlund <simon.tornlund@ecsolutions.se>
2022-12-06 19:08:13 +0100

    Refactor

--- days_rest/src/days/day_6.rs
+++ days_rest/src/days/day_6.rs
@@ -1,28 +1,23 @@
-// For each char
-// If that char + 3 next ones are unique.
-// return index of last char needed (+1)
-
 pub fn day6_part_1() -> i32 {
     let data = get_input();
+    return get_index_unique(data, 4);
+}
 
+fn get_index_unique(string: String, target: usize) -> i32 {
     let mut ans = 0;
     let mut unique: Vec<char> = Vec::new();
-    let lookup_copy: Vec<_> = data.chars().collect();
+    let lookup_copy: Vec<char> = string.chars().collect();
 
-
-    for (i, _) in data.chars().into_iter().enumerate() {
-        unique.push(*lookup_copy.get(i).unwrap());
-        unique.push(*lookup_copy.get(i + 1).unwrap());
-        unique.push(*lookup_copy.get(i + 2).unwrap());
-        unique.push(*lookup_copy.get(i + 3).unwrap());
+    for (i, _) in string.chars().into_iter().enumerate() {
+        for k in 0..target {
+            unique.push(*lookup_copy.get(i + k).unwrap());
+        }
         
-        // Values need to be sorted in order to dedupe..
         unique.sort();
         unique.dedup();
 
-        // If we still have 4 chars after deduping, we got the answer.
-        if unique.len() == 4 {
-            ans = i+4;
+        if unique.len() == target {
+            ans = i+target;
             break;
         }
         unique.clear();
@@ -31,35 +26,10 @@ pub fn day6_part_1() -> i32 {
     ans as i32
 }
 
+
 pub fn day6_part_2() -> i32 {
     let data = get_input();
-
-    let mut ans = 0;
-    let mut unique: Vec<char> = Vec::new();
-    let lookup_copy: Vec<_> = data.chars().collect();
-
-
-    for (i, _) in data.chars().into_iter().enumerate() {
-
-
-
-        for k in 0..14 {
-            unique.push(*lookup_copy.get(i + k).unwrap());
-        }
-        
-        // Values need to be sorted in order to dedupe..
-        unique.sort();
-        unique.dedup();
-
-        // If we still have 14 chars after deduping, we got the answer.
-        if unique.len() == 14 {
-            ans = i+14;
-            break;
-        }
-        unique.clear();
-    }
-
-    ans as i32
+    return get_index_unique(data, 14);
 }
 
 fn get_input() -> String {
