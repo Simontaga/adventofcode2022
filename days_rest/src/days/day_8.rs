b5ba479248124f46d1ec0b96ca7ce31443ba6c57
Simon Törnlund <simon.tornlund@ecsolutions.se>
2022-12-08 20:18:25 +0100

    Refactor

--- days_rest/src/days/day_8.rs
+++ days_rest/src/days/day_8.rs
@@ -1,6 +1,4 @@
-
 use take_until::TakeUntilExt;
-
 
 #[derive(Debug, Copy, Clone)]
 struct Tree {
@@ -11,16 +9,11 @@ struct Tree {
 
 pub fn day8_part_1() -> i32 {
     let input = get_input();
-    let lines: Vec<&str> = input.lines().collect();
+    let lines = input.lines();
     let mut trees: Vec<Tree> = Vec::new();
-    let max_y = lines.len() - 1;
-    let mut max_x = 0;
 
-    for (y, line) in lines.into_iter().enumerate() {
+    for (y, line) in lines.collect::<Vec<&str>>().into_iter().enumerate() {
         for (x, height) in line.chars().enumerate() {
-            if max_x < x {
-                max_x = x;
-            }
             trees.push(Tree {
                 x: x as u32,
                 y: y as u32,
@@ -29,7 +22,7 @@ pub fn day8_part_1() -> i32 {
         }
     }
     trees.reverse();
-    count_trees_visible(trees, max_y as u32, max_x as u32)
+    count_trees_visible(trees)
 }
 
 pub fn day8_part_2() -> i32 {
@@ -46,12 +39,12 @@ pub fn day8_part_2() -> i32 {
             })
         }
     }
-    
+
     count_trees_scenic_score(trees)
 }
 
 // Extremely inefficent, not blazingly fast.
-fn count_trees_visible(trees: Vec<Tree>, _max_y: u32, _max_x: u32) -> i32 {
+fn count_trees_visible(trees: Vec<Tree>) -> i32 {
     let trees_clone = trees.clone();
     let mut score = 0;
 
@@ -76,7 +69,7 @@ fn count_trees_visible(trees: Vec<Tree>, _max_y: u32, _max_x: u32) -> i32 {
             .iter()
             .filter(|x| x.height >= tree.height && x.y < tree.y && x.x == tree.x);
 
-        if up.count() == 0 || down.count() == 0 || left.count() == 0|| right.count() == 0 {
+        if up.count() == 0 || down.count() == 0 || left.count() == 0 || right.count() == 0 {
             score += 1;
         }
     }
@@ -105,7 +98,6 @@ fn count_trees_scenic_score(trees: Vec<Tree>) -> i32 {
             .iter()
             .filter(|x| x.y > tree.y && x.x == tree.x)
             .take_until(|x| x.height >= tree.height);
-
 
         let up = trees_clone
             .iter()
